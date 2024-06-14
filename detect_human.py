aa7322ed8736b6347a79ca398e99311e98145dc8
gadgelogger <sarayou273142910@gmail.com>
2024-06-14 17:59:08 +0900

    feat:タイムスタンプを書き込むように処理を追加

--- detect_human.py
+++ detect_human.py
@@ -4,19 +4,19 @@ import os
 from supabase import create_client, Client
 import time
 from config import supabase_url, supabase_key
+from datetime import datetime
 
-
-# Supabaseの設定
+# Supabaseの設定(APIキーを取得する)
 url: str = supabase_url
 key: str = supabase_key
 supabase: Client = create_client(url, key)
 
-# テーブル名とカラム情報
+# superbase上のテーブル名とカラム情報を指定する
 table_name = "count"
 column_name = "person"
 column_type = "int8"
 
-# 保存先のディレクトリ
+# 画像の保存先のディレクトリを指定
 save_directory = "captured_images"
 
 # 保存先のディレクトリが存在しない場合は作成する
@@ -51,17 +51,20 @@ human_results = results_df[results_df['name'] == 'person']
 # 人数をカウント
 person_count = len(human_results)
 
+# 現在のタイムスタンプを取得
+current_timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
 # 既存のレコードを確認する
 query = supabase.table(table_name).select('*').limit(1).execute()
 existing_data = query.data
 
 if existing_data:
     # 既存のレコードがある場合は更新する
-    res = supabase.table(table_name).update({"person": person_count}).eq('person', existing_data[0]['person']).execute()
+    res = supabase.table(table_name).update({"person": person_count, "time": current_timestamp}).eq('person', existing_data[0]['person']).execute()
     print(f"Updated data: {res}")
 else:
     # 既存のレコードがない場合は挿入する
-    data = {column_name: person_count}
+    data = {column_name: person_count, "time": current_timestamp}
     res = supabase.table(table_name).insert(data).execute()
     print(f"Inserted data: {res}")
 
@@ -74,6 +77,8 @@ for index, row in human_results.iterrows():
 image_path = os.path.join(save_directory, "captured_image.jpg")
 cv2.imwrite(image_path, frame)
 print(f"写真を {image_path} に保存しました")
+print(f"人数: {person_count}")
+print(f"タイムスタンプ: {current_timestamp}")
 
 # リソースを解放
 cap.release()
